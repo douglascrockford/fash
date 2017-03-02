c893c670f3056b8c0243445e3ad1bae641b9949e
Douglas Crockford <douglas@crockford.com>
2017-03-02 15:16:55 -0800

    64

--- srash64.h
+++ srash64.h
@@ -3,7 +3,7 @@
 The srash64 header file. This is the companion to srash64.asm.
 
 
-2017-02-01
+2017-03-02
 Public Domain
 
 No warranty.
@@ -14,4 +14,4 @@ typedef unsigned long long uint64;
 
 extern void srash64_seed(uint64 *seeds);
 extern uint64 srash64();
-extern void srash_dump(uint64 *seeds);
+extern void srash64_dump(uint64 *seeds);
